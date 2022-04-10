b6c54cdb480cd19b04feb4888cffdc7501398223
GoatBandit <connoreaster@gmail.com>
2022-04-10 10:53:42 +0100

    Tactical Scale InnerSphere

--- CCTP Unity Files/Assets/Scripts/Gameplay/Mechanics/Trigger/Trigger.cs
+++ CCTP Unity Files/Assets/Scripts/Gameplay/Mechanics/Trigger/Trigger.cs
@@ -7,6 +7,16 @@ public class Trigger : MonoBehaviour
     public bool isTriggered = false;
     public int result = 0;
 
+    private void Update() 
+    {
+        if (result == 5 && isTriggered) 
+        {
+            PlayerController playerScript = GameObject.FindGameObjectWithTag ("Player").GetComponent <PlayerController>();
+            playerScript.insideSphere = true;
+            playerScript.insideSphereRadius = 15;
+        }
+    }
+
     private void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Player" || other.tag == "TriggerIt")
